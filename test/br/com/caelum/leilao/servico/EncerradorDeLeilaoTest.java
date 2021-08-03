61eb558b075035de602605ddc62fe5f86bbce0fc
Rodolpho Alves <rodolpho.alves@zup.com.br>
2021-08-03 20:56:02 -0300

    exercicio nao encerra leiloes começados ontem

--- test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -6,12 +6,9 @@ import br.com.caelum.leilao.infra.dao.LeilaoDao;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.List;
+import java.util.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -52,5 +49,35 @@ public class EncerradorDeLeilaoTest {
         assertEquals(2, encerrador.getTotalEncerrados());
         assertTrue(leilao1.isEncerrado());
         assertTrue(leilao2.isEncerrado());
+    }
+
+    @Test
+    public void naoEncerraLeiloesComecadosOntem() {
+        Calendar ontem = Calendar.getInstance();
+        ontem.add(Calendar.DAY_OF_MONTH, -1);
+
+        Leilao leilao1 = new CriadorDeLeilao()
+                .para("tv")
+                .naData(ontem)
+                .constroi();
+
+        Leilao leilao2 = new CriadorDeLeilao()
+                .para("geladeira")
+                .naData(ontem)
+                .constroi();
+
+        dao.salva(leilao1);
+        dao.salva(leilao2);
+
+        List<Leilao> leiloesDeOntem = Arrays.asList(leilao1, leilao2);
+
+        when(dao.correntes()).thenReturn(leiloesDeOntem);
+
+        EncerradorDeLeilao encerrador = new EncerradorDeLeilao(dao);
+        encerrador.encerra();
+
+        assertEquals(encerrador.getTotalEncerrados(), 0);
+        assertFalse(leilao1.isEncerrado());
+        assertFalse(leilao2.isEncerrado());
     }
 }
